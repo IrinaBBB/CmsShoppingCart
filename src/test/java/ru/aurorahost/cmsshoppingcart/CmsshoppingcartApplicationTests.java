e0d22007e4d27d29e5fe4b620fb43f9890d66111
IrinaBBB <balkarovairina@gmail.com>
2021-04-05 11:48:37 +0200

    initial commit

---
+++ src/test/java/ru/aurorahost/cmsshoppingcart/CmsshoppingcartApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aurorahost.cmsshoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsshoppingcartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
